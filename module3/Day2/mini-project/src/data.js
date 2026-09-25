dc95b4645e637285ff36b9ae20b7130db9224fde
hiwi <hiwotketema701@gmail.com>
2026-09-24 23:24:59 -0700

    Add menu data array

---
+++ module3/Day2/mini-project/src/data.js
@@ -0,0 +1,7 @@
+export const dishes = [
+  { id: 1, name: "Doro Wat", price: 240, category: "Stew", spicy: true },
+  { id: 2, name: "Kitfo", price: 280, category: "Meat", spicy: true },
+  { id: 3, name: "Shiro", price: 150, category: "Stew", spicy: false },
+  { id: 4, name: "Tibs", price: 220, category: "Meat", spicy: false },
+  { id: 5, name: "Firfir", price: 130, category: "Breakfast", spicy: false },
+];
